646defcffccd2c5f779f42ec34c8fbd678925526
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-09-11 15:08:52 -0700

    add buttons and changing background color using JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(function() {
   $(".clickable").click(function() {
-    $(".initially-showing").fadeToggle();
-    $(".initially-hidden").fadeToggle();
+    $(".initially-showing").toggle();
+    $(".initially-hidden").toggle();
+  });
+
+  $("button#purple").click(function(){
+    $("body").removeClass();
     $("body").addClass("purple-background");
+  });
+
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#white").click(function(){
+    $("body").removeClass();
+    $("body").addClass("white-background");
   });
 });
